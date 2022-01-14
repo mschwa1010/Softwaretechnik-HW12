6021b88b09ae2abb9ed97c06d33f14e9cdb6b5f2
mschwa1010 <80753072+mschwa1010@users.noreply.github.com>
2022-01-14 19:09:13 +0100

    init

---
+++ src/main/java/com/softwaretechnik/SoftwaretechnikHw12Application.java
@@ -0,0 +1,13 @@
+package com.softwaretechnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwaretechnikHw12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwaretechnikHw12Application.class, args);
+	}
+
+}
